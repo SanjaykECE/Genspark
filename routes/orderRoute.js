6da291e68fb2ddaff9c0fc517cd349ed49549608
SanjaykECE <sanjay.k2021ecec@sece.ac.in>
2025-03-13 15:07:54 +0530

    updated

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,10 +1,8 @@
-const createOrder = require("../controllers/createOrder");
 const getAllOrder = require("../controllers/getOrder");
 const getUserOrder = require("../controllers/getUserOrder");
 const { auth } = require("../middlewares/auth");
 const router = require("express").Router();
 
-router.post("/", auth, createOrder);
 router.get("/", getAllOrder);
 router.get("/user", auth, getUserOrder);
 
